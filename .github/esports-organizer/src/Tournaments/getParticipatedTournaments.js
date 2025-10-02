e0c0aad4b01506b1c9663b84619d0d98c9f62a4c
SebastianSuarez0429 <sebastian.suarez4@upr.edu>
2025-10-02 16:40:50 -0400

    made the getParticipadedTournaments function

---
+++ .github/esports-organizer/src/Tournaments/getParticipatedTournaments.js
@@ -0,0 +1,29 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../config/firestore';
+
+
+
+// All console logs are for debugging purposes to understand the Firestore data and see if we are fetching it correctly. :)
+// The debugging would be seen in the browser console.
+async function getParticipatedTournaments() {
+    const querySnapshot = await getDocs(collection(db, "User"));
+    
+    console.log("=== FIRESTORE DATA STRUCTURE ===");
+    console.log("Total documents:", querySnapshot.size);
+    
+    querySnapshot.forEach((doc) => {
+      console.log("Document ID:", doc.id);
+      console.log("Document data:", doc.data());
+      console.log("All fields in this document:", Object.keys(doc.data()));
+      console.log("---");
+    });
+    
+    const User = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+    
+    return User;
+}
+
+export default getParticipatedTournaments;
