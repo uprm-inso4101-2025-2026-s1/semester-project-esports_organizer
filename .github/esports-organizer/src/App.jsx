8aece35c9072bfe2da95546bd668e0baa9ad157e
VictorSantos6 <victor.santos6@upr.edu>
2025-10-02 20:39:22 -0400

    Tournament Brackets Page

--- .github/esports-organizer/src/App.jsx
+++ .github/esports-organizer/src/App.jsx
@@ -2,15 +2,10 @@ import { useNavigate } from "react-router-dom";
 import Button from "./components/shared/Button";
 
 function App() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   return (
     <>
-      <div>Hello World! </div>
-      <Button
-        text={"User Authentication Pages"}
-        onClick={() => navigate("/signup")}
-      />
     </>
   );
 }
