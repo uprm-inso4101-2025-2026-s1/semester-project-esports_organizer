edabe113ce93f9d19a28bb6d2a83df57dc379bb0
seba-15 <sebastian.santiago9@upr.edu>
2025-10-04 00:35:54 -0700

    Finished with landing page

--- .github/esports-organizer/src/pages/LandingPage.jsx
+++ .github/esports-organizer/src/pages/LandingPage.jsx
@@ -12,6 +12,7 @@ import {
 } from "../assets/Icons"; // not used here directly, but keeping for reference
 
 // Demo images
+import logo from "../assets/images/Logo.png";
 import fortniteCover from "../assets/images/Fortnite.png";
 import valorantCover from "../assets/images/Valorant.png";
 import apexCover from "../assets/images/Apex.png";
@@ -31,7 +32,7 @@ export default function LandingPage() {
       gameName: "Fortnite",
       badge: "PLAYOFFS",
     },
-    {
+    { 
       id: 2,
       title: "Trios Arena Rush",
       imageUrl: valorantCover,
@@ -78,22 +79,28 @@ export default function LandingPage() {
   return (
     <div className="lp">
       {/* Top nav / header */}
-     <header className="lp__nav container">
+      <header className="lp__nav container">
         <div className="lp__brand">
           {/* Logo placeholder */}
-          <div
-            className="lp__logo-skeleton"
-            aria-label="App logo placeholder"
-          />
+          <div className="lp__logo-skeleton" aria-label="App logo placeholder">
+            <img
+              src={logo}
+              alt="Esports Organizer logo"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+                display: "block",
+              }}
+            />
+          </div>
           <span className="lp__brand-text">esports organizer</span>
         </div>
         <nav className="lp__auth" aria-label="Authentication">
           <a href="/login" className="lp__auth-link">
             Login
           </a>
-          <span aria-hidden="true">
-            /
-          </span>
+          <span aria-hidden="true">/</span>
           <a href="/signup" className="lp__auth-link">
             Sign Up
           </a>
@@ -102,29 +109,40 @@ export default function LandingPage() {
 
       {/* Hero section */}
       <section className="lp__hero container">
-        <div className="lp__hero-left">
-          <h1 className="lp__headline">
-            <span className="lp__hl lp__hl--primary">Play</span> More.
-            <br />
-            <span className="lp__hl lp__hl--secondary">Connect</span> More.
-            <br />
-            <span className="lp__hl lp__hl--primary">Achieve</span> More.
-          </h1>
-          <p className="lp__sub">
-            with the{" "}
-            <a href="#app" className="lp__link">
-              esports organizer
-            </a>{" "}
-            app
-          </p>
-        </div>
+        <div className="lp__hero-top">
+          <div className="lp__hero-left">
+            <h1 className="lp__headline">
+              <span className="lp__hl lp__hl--primary">Play</span> More.
+              <br />
+              <span className="lp__hl lp__hl--primary">Connect</span> More.
+              <br />
+              <span className="lp__hl lp__hl--primary">Achieve</span> More.
+            </h1>
+            <p className="lp__sub">
+              with the{" "}
+              <a href="#app" className="lp__link">
+                esports organizer
+              </a>{" "}
+              app
+            </p>
+          </div>
 
-        {/* Right graphic area (reserve space for logo/illustration) */}
-        <div className="lp__hero-right">
-          <div className="lp__illustration-skeleton" aria-hidden />
+          {/* Right graphic area (reserve space for logo/illustration) */}
+          <div className="lp__hero-right">
+            <div className="lp__illustration-skeleton">
+              <img
+                src={logo}
+                alt="Esports Organizer logo"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "contain",
+                  display: "block",
+                }}
+              />
+            </div>
+          </div>
         </div>
-
-        {/* BELOW ROW: copy + CTA spanning full width */}
         <div className="lp__hero-below">
           <p className="lp__copy">
             The all-in-one hub for esports players and esport enthusiasts. Here,
