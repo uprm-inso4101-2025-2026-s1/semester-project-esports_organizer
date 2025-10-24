9d2116fb29d4f38b6b0de31a694b751bf02359ca
Programian <ian.rivera21@upr.edu>
2025-10-23 22:43:17 -0400

    cleaned up the CommunityCreation.js file, should mostly be done except for saving the community object in the db

--- .github/esports-organizer/src/Comm-Social/CommunityCreation.js
+++ .github/esports-organizer/src/Comm-Social/CommunityCreation.js
@@ -8,11 +8,12 @@ import {Community} from "./Community.js";
  * @argument {string} description argument string community description
  * @argument {string} admin ID of the account that created the community
  * @argument {string[]} tags argument array of tags in a community
+ * @argument {string} game the game around which the community is based
  * @argument {string} location where the community is based in
  * @argument {string} icon community icon image location string
  * @argument {string} banner community banner image location string
  */
-export function createCommunity(name, description, admin, tags, location, icon, banner){
+export function createCommunity(name, description, admin, tags, game, location, icon, banner){
 
     //Members will start as an empty array to house the ids of future members with the admin id added as first member
     members = [];
@@ -24,15 +25,8 @@ export function createCommunity(name, description, admin, tags, location, icon, 
     //Generate a random UUID
     id = crypto.randomUUID();
 
-        //TO DO: localization, icon and banner properties are not present yet in the constructor
-        //location (string, selected or entered by the user, where the community is present or based in)
-        //icon (image location)
-        //banner (image location)
-        /*icon and banner require storing their respective images as a variable pointing to their 
-        source to load later as needed? These images would have to be saved somewhere in the repository
-        or do we have an image database or could have?*/
-
-    communityObject = Community(name, description, admin, members, posts, tags, dateCreated, id);
+    communityObject = Community(name, description, admin, members, posts, tags, id, dateCreated, game, location, 
+    icon, banner);
 
     //TO DO: send community object to database
 
